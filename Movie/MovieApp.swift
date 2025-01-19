7c410d528ac9012232d70aa1293512e32052a328
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2025-01-19 15:15:35 +0300

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Deem Ibrahim on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
