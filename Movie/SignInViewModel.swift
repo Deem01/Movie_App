e74c69e9af09656a05abd96aa414c94d6dd69dcb
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2025-01-30 15:17:28 +0300

    the app

---
+++ Movie/SignInViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewModel.swift
+//  Movie
+//
+//  Created by Deem Ibrahim on 19/01/2025.
+//
+
+import SwiftUI
+
+struct SignInViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInViewModel()
+}
